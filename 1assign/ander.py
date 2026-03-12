c38b40e098917973714b94711bce36e7465321bf
ander.arrondo.e <ander.arrondo.e@opendeusto.es>
2026-03-12 22:07:11 +0100

    tensorboard visualization of multiple metrics and improved code

--- 1assign/ander.py
+++ 1assign/ander.py
@@ -2,7 +2,9 @@
 # LIBRARIES
 import torch
 import torch.nn as nn
+from torchmetrics import NormalizedRootMeanSquaredError, MeanAbsoluteError, MeanAbsolutePercentageError
 from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.tensorboard import SummaryWriter
 
 import numpy as np
 import pandas as pd
@@ -11,18 +13,19 @@ import seaborn as sns
 import plotly.express as px
 import matplotlib.pyplot as plt
 
-
 from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import StandardScaler
 
 
 # CONFIGS
 csv_path: str = "./data/insurance.csv"
 device: str = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-show_plots: bool = True
-train_model: bool = False
+show_plots: bool = False
+train_model: bool = True
 batch_size: int = 32
-epochs: int = 30
+epochs: int = 20
 lr: float = 1e-3
+writer = SummaryWriter("runs/insurance")
 
 random_seed: int = 42
 torch.manual_seed(random_seed)
@@ -44,7 +47,7 @@ categorical_cols = ["sex", "smoker", "region"]
 for category in categorical_cols:
     raw_df[category] = pd.Categorical(raw_df[category])
 
-df: pd.DataFrame = pd.get_dummies(raw_df, drop_first=True, dtype=int)
+df: pd.DataFrame = pd.get_dummies(raw_df, drop_first=False, dtype=int)
 print(df.head())
 print()
 df.info()
@@ -129,13 +132,23 @@ def split_data(
     X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
     X_test, X_val, y_test, y_val = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
+    x_scaler = StandardScaler()
+    X_train = x_scaler.fit_transform(X_train)
+    X_test = x_scaler.transform(X_test)
+    X_val = x_scaler.transform(X_val)
+
+    y_scaler = StandardScaler()
+    y_train = y_scaler.fit_transform(y_train.reshape(-1, 1))
+    y_test = y_scaler.transform(y_test.reshape(-1, 1))
+    y_val = y_scaler.transform(y_val.reshape(-1, 1))
+
     train_loader = create_loader(X_train, y_train, batch_size, shuffle=True)
     test_loader = create_loader(X_test, y_test, batch_size)
     val_loader = create_loader(X_val, y_val, batch_size)
 
-    return train_loader, test_loader, val_loader
+    return train_loader, test_loader, val_loader, x_scaler, y_scaler
 
-train_loader, test_loader, val_loader = split_data(df, batch_size=batch_size)
+train_loader, test_loader, val_loader, x_scaler, y_scaler = split_data(df, batch_size=batch_size)
 for X, y in train_loader:
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
@@ -147,10 +160,21 @@ class InsuranceModel(nn.Module):
         super().__init__()
 
         self.layers = nn.Sequential(
-            nn.Linear(8, 32),
+            nn.Linear(11, 128),
             nn.ReLU(),
+
+            nn.Dropout(0.2),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+
+            nn.Dropout(0.2),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+
+            nn.Dropout(0.2),
             nn.Linear(32, 16),
             nn.ReLU(),
+
             nn.Linear(16, 1)
         )
 
@@ -166,7 +190,9 @@ def train(
         dataloader: DataLoader,
         model: nn.Module,
         loss_fn: nn.Module, 
-        optimizer: torch.optim.Optimizer
+        optimizer: torch.optim.Optimizer,
+        writer: SummaryWriter,
+        epoch: int
         ) -> None:
     size = len(dataloader.dataset)
     model.train()
@@ -176,6 +202,10 @@ def train(
 
         pred = model(X)
         loss = loss_fn(pred, y)
+
+        global_step = epoch * len(dataloader) + batch
+
+        writer.add_scalar("Train/Loss", loss.item(), global_step)
 
         optimizer.zero_grad()
         loss.backward()
@@ -189,25 +219,46 @@ def train(
 def test(
         dataloader: DataLoader,
         model: nn.Module,
-        loss_fn: nn.Module
+        loss_fn: nn.Module,
+        y_scaler: StandardScaler,
+        writer: SummaryWriter,
+        epoch: int
         ) -> None:
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
     model.eval()
+    
+    nrmse = NormalizedRootMeanSquaredError().to(device)
+    mae = MeanAbsoluteError().to(device)
+    mape = MeanAbsolutePercentageError().to(device)
 
     test_loss = 0
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
-            pred = model(X)
+            pred = model(X) 
             test_loss += loss_fn(pred, y).item()
-    test_loss /= num_batches
 
-    print(f"Test Error: \n Avg loss: {test_loss:8f}\n")
+            pred_original = torch.tensor(y_scaler.inverse_transform(pred.cpu().numpy()), device=device)
+            y_original = torch.tensor(y_scaler.inverse_transform(y.cpu().numpy()), device=device)
+            nrmse.update(pred_original, y_original)
+            mae.update(pred_original, y_original)
+            mape.update(pred_original, y_original)
+    test_loss /= len(dataloader)
+
+    epoch_nrmse = nrmse.compute()
+    epoch_mae = mae.compute()
+    epoch_mape = mape.compute()
+
+    writer.add_scalar("Test/Loss", test_loss, epoch)
+    writer.add_scalar("Test/NRMSE", epoch_nrmse, epoch)
+    writer.add_scalar("Test/MAE", epoch_mae, epoch)
+    writer.add_scalar("Test/MAPE", epoch_mape, epoch)
+
+    print(f"Test Error: \n Avg loss: {test_loss:8f}\n Avg MAPE: {epoch_mape:8f}\n")
+    mape.reset()
 
 if train_model:
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
-        train(train_loader, model, loss_fn, optimizer)
-        test(test_loader, model, loss_fn)
+        train(train_loader, model, loss_fn, optimizer, writer, t)
+        test(test_loader, model, loss_fn, y_scaler, writer, t)
     print("Done!")
