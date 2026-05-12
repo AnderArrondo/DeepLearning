4fda3103d0615b0b14a0ac2962446b102a53d417
ander.arrondo.e <ander.arrondo.e@opendeusto.es>
2026-05-12 09:48:40 +0200

    more metrics included

--- 3assign/config.py
+++ 3assign/config.py
@@ -4,6 +4,7 @@ import torch
 ORIG_CSV: str = "./3assign/data/tweet_emotions.csv"
 DATA_DIR: str = "./3assign/data/"
 PLOTS_DIR: str = "./3assign/plots/"
+RESULTS_DIR: str = "./3assign/results/"
 
 DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL: str = "roberta-large-mnli"
--- 3assign/zero_shot.py
+++ 3assign/zero_shot.py
@@ -3,15 +3,24 @@
 # Libraries
 #######
 
-from config import DATA_DIR, PLOTS_DIR, MODEL, ZERO_SHOT_BATCH_SIZE, DEVICE
+from config import (
+    DATA_DIR, PLOTS_DIR, RESULTS_DIR,
+    MODEL, ZERO_SHOT_BATCH_SIZE, DEVICE
+)
 
+import json
 import torch
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from transformers import pipeline
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import (
+    confusion_matrix, ConfusionMatrixDisplay,
+    accuracy_score, balanced_accuracy_score,
+    precision_score, recall_score, f1_score,
+    classification_report
 
+)
 torch.backends.cudnn.benchmark = True
 
 #######
@@ -46,3 +55,22 @@ disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=sorted(list(set
 disp.plot(cmap="Blues")
 plt.savefig(PLOTS_DIR + "zero_shot_conf_matrix.png")
 
+metrics = {
+    "acc": accuracy_score(y, preds),
+    "balanced_acc": balanced_accuracy_score(y, preds),
+    
+    "f1_macro": f1_score(y, preds, average="macro"),
+    "f1_weighted": f1_score(y, preds, average="weighted"),
+
+    "precision_macro": precision_score(y, preds, average="macro"),
+    "precision_weighted": precision_score(y, preds, average="weighted"),
+
+    "recall_macro": recall_score(y, preds, average="macro"),
+    "recall_weighted": recall_score(y, preds, average="weighted"),
+
+    "report": classification_report(y, preds, output_dict=True)
+}
+
+with open(str(RESULTS_DIR + "zero_shot.json"), "w") as f:
+    json.dump(metrics, f)
+    
